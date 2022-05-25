09800c75c86f1d675f25375f38fa3df7e40c8d24
Tobias Meyer <52013820+Hope-IT-Works@users.noreply.github.com>
2022-05-25 21:36:54 +0200

    Create

---
+++ src/c#/Explorestart/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace Explorestart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] ExplorerProcess = Process.GetProcessesByName("explorer");
+            foreach (Process p in ExplorerProcess)
+            {
+                p.Kill();
+            }
+            ExplorerProcess = Process.GetProcessesByName("explorer");
+            if (ExplorerProcess.Length == 0)
+            {
+                Process.Start("explorer.exe");
+            }
+        }
+    }
+}
